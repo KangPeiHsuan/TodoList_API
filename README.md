ed4b23dd96a4c7a583582546887751ab98ae09cd
KangPei <s522tw@gmail.com>
2024-11-01 16:07:39 +0800

    feat: create User Controller

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 ## 復刻一個 RESTFUL API 來管理 todos 列表
 > 參考 https://todoo.5xcamp.us/api-docs/index.html 的 API 文件
  
-### 實現功能
-### Users
+#### 可使用路由：
+#### Users
 | 方法     | 路由 | 描述  |
 | --- | --- | --- |
 | POST    | /users/sign_in | 使用者登入 |
 | DELETE  | /users/sign_out | 使用者登出 |
 
-### Todos
+#### Todos
 | 方法     | 路由 | 描述  |
 | --- | --- | --- |
 | GET    | /todos | 取得 TODO 列表 |
@@ -16,11 +16,11 @@
 | PUT  | /todos/{id} | 修改 TODO |
 | DELETE  | /todos/{id} | 刪除 TODO |
 | PATCH  | /todos/{id}/toggle | TODO 完成 / 已完成切換 |
-
+---
 ### 專案的運行方式
 ### 專案結構
 ### 實作思路
 ### 遇到的挑戰和解決方案
-
+---
 ### 實作筆記
 [[note] Todos API 實作](https://hackmd.io/@kangpei/HJ_YC3hlJl)
---
+++ TodoAPI/Controllers/UserController.cs
@@ -0,0 +1,79 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TodoAPI.Dtos;
+using TodoAPI.Models;
+using TodoAPI.Providers;
+using Microsoft.AspNetCore.Identity;
+
+namespace TodoAPI.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        private readonly JwtProvider _jwtProvider;
+        private readonly TodoContext _todoContext;
+
+        public UserController(JwtProvider jwtProvider, TodoContext todoContext)
+        {
+            _jwtProvider = jwtProvider;
+            _todoContext = todoContext;
+        }
+
+        // POST users/sign_in
+        [HttpPost("sign_in")]
+        public IActionResult SignIn([FromBody] LoginDto loginDto)
+        {
+            if (loginDto.Email == "user@example.com" || loginDto.Email == null)
+            {
+                return BadRequest("請輸入您的電子信箱");
+            }
+
+            var user = _todoContext.User.SingleOrDefault(u => u.Email == loginDto.Email);
+            if (user == null)
+            {
+                var newUser = new User { Email = loginDto.Email };
+                var passwordHasher = new PasswordHasher<User>();
+                newUser.PasswordHash = passwordHasher.HashPassword(newUser, loginDto.Password);
+
+                _todoContext.Add(newUser);
+                _todoContext.SaveChanges();
+
+                var token = _jwtProvider.GenerateToken(loginDto);
+
+                // 將 token 放入 response headers 內
+                Response.Headers.Add("Authorization", $"Bearer {token}");
+
+                return Ok(loginDto);
+            }
+            else
+            {
+                var passwordHasher = new PasswordHasher<User>();
+                var passwordVerificationResult = passwordHasher.VerifyHashedPassword(user, user.PasswordHash, loginDto.Password);
+
+                if (passwordVerificationResult == PasswordVerificationResult.Success)
+                {
+                    var token = _jwtProvider.GenerateToken(loginDto);
+
+                    // 將 token 放入 response headers 內
+                    Response.Headers.Add("Authorization", $"Bearer {token}");
+
+                    return Ok(loginDto);
+                }
+                else
+                {
+                    return BadRequest(new { message = "密碼不正確" });
+                }
+
+            }
+        }
+
+        // DELETE users/sign_in
+        [HttpDelete("sign_out")]
+        public IActionResult SignOut()
+        {
+            return Ok(new { message = "登出成功" });
+        }
+    }
+}
+
